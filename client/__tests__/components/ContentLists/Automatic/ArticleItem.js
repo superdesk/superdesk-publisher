af7b78e8f09353c8855c29b40dd3d7266c7bf10e
Tomasz Rondio <tomasz.rondio@gmail.com>
2019-08-09 11:14:38 +0200

    content lists tests

---
+++ client/__tests__/components/ContentLists/Automatic/ArticleItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ArticleItem from "../../../../components/ContentLists/Automatic/ArticleItem";
+import { render } from "@testing-library/react";
+
+describe("ContentLists/Automatic/ArticleItem", () => {
+  const item = {
+    content: {
+      title: "test",
+      route: {
+        name: "route"
+      },
+      authors: [{ name: "author" }],
+      published_at: "2017-07-26T13:43:33+00:00",
+      article_statistics: {
+        page_views_number: 10,
+        internal_click_rate: 5,
+        impressions_number: 15
+      }
+    }
+  };
+
+  it("renders correctly", () => {
+    const { container } = render(
+      <ArticleItem
+        item={item}
+        style={{}}
+        openPreview={jest.fn()}
+        previewItem={{}}
+        pinUnpin={jest.fn()}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
---
+++ client/__tests__/components/ContentLists/Manual/Manual.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Manual from "../../../../components/ContentLists/Manual/Manual";
+import { render, waitForElement } from "@testing-library/react";
+import axios from "axios";
+
+import Publisher from "../../../../__mocks__/publisher";
+
+const publisher = new Publisher();
+
+let lists = [];
+publisher.queryLists().then(items => (lists = items));
+
+jest.mock("axios");
+
+let api = () => {};
+
+api.users = {};
+api.users.query = function() {
+  return new Promise((resolve, reject) => {
+    resolve({
+      _items: [{ is_author: true, display_name: "test author" }],
+      _links: {}
+    });
+  });
+};
+
+describe("ContentLists/Manual/Manual", () => {
+  it("renders properly", async () => {
+    const { container, getByText } = render(
+      <Manual
+        list={lists[0]}
+        lists={lists}
+        publisher={publisher}
+        listEdit={jest.fn()}
+        onListUpdate={jest.fn()}
+        api={api}
+        toggleFilters={jest.fn()}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
