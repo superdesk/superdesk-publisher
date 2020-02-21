1b78ca5261998fb60d85062623d3d35aba0e01c1
mehdi <dridignu@gmail.com>
2020-02-21 12:04:51 +0100

    remvove old directive

--- client/directives/index.js
+++ client/directives/index.js
@@ -1,3 +1,2 @@
 export { SiteWizardDirective } from './siteWizard/SiteWizardDirective';
 export { ThemeManagerDirective } from './themeManager/ThemeManagerDirective';
-export { ListContentListsDirective } from './listContentLists/ListContentListsDirective';
