46de680c416970258bb7e173acb681ce63897388
Ljuba Ranković <ljubar@gmail.com>
2026-04-01 11:12:47 +0200

    fixed tests

--- client/__tests__/components/UI/SearchBar.js
+++ client/__tests__/components/UI/SearchBar.js
@@ -7,6 +7,14 @@ describe("UI/SearchBar", () => {
     const { container } = render(
       <SearchBar onChange={jest.fn()} value="testvalue" />
     );
+    // input is hidden until search is toggled open
+    expect(
+      container.querySelector("input[value='testvalue']")
+    ).not.toBeInTheDocument();
+
+    // click the search icon to open
+    fireEvent.click(container.querySelector(".trigger-icon"));
+
     expect(
       container.querySelector("input[value='testvalue']")
     ).toBeInTheDocument();
